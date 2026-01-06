7b0576f0871c8c8fd6c2210d790211de4604c10c
Valerie Vozza <valerievozza@gmail.com>
2026-01-05 19:51:12 -0800

    fix resources page

--- src/pages/Resources.vue
+++ src/pages/Resources.vue
@@ -1,18 +1,28 @@
 <template>
   <v-container class="d-flex flex-column">
     <h1 class="text-primary">Resources</h1>
-    <v-card>
-      <v-list>
-        <v-list-item>
-          <v-list-item-title>Letter of Demands</v-list-item-title>
-          <a href="">View draft</a>
-        </v-list-item>
-
-        <v-list-item>
-          <v-list-item-title>Portland Tenants United</v-list-item-title>
-          <a href="https://www.pdxtu.org/" target="_blank">Go to site</a>
-        </v-list-item>
-      </v-list>
+    <v-card
+      v-for="resource in resources"
+      :key="resource.title"
+      class="mt-4"
+      :title="resource.title"
+    >
+      <v-card-text>
+        <a :href="resource.link" target="_blank">{{ resource.link }}</a>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
+
+<script setup lang="ts">
+  const resources = [
+    {
+      title: 'Letter of Demands',
+      link: '[Insert link here]',
+    },
+    {
+      title: 'Portland Tenants United',
+      link: 'https://www.pdxtu.org/',
+    },
+  ]
+</script>
