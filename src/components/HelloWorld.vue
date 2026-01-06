8a0a865808b36b7cdaa3da582c7228116ec5d29f
Valerie Vozza <valerievozza@gmail.com>
2026-01-05 18:02:05 -0800

    update main page links

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <v-container class="fill-height" max-width="1200">
     <div>
       <v-img
-        class="mb-4"
+        class="mb-6"
         cover
         gradient="to top, rgba(0,0,0,.5), rgba(0,0,0,.5)"
         height="260"
@@ -14,44 +14,15 @@
         </div>
       </v-img>
       <v-row>
-        <v-col cols="12">
+        <v-col v-for="link in links" :key="link.route" cols="12">
           <v-card
             class="py-4"
             color="surface-variant"
-            prepend-icon="mdi-rocket-launch-outline"
-            rounded="lg"
-            variant="tonal"
-          >
-            <template #image>
-              <v-img position="top right" />
-            </template>
-
-            <template #title>
-              <h2 class="text-h5 font-weight-bold">
-                Get started
-              </h2>
-            </template>
-
-            <template #subtitle>
-              <div class="text-subtitle-1">
-                Change this page by updating <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in <v-kbd>components/HelloWorld.vue</v-kbd>.
-              </div>
-            </template>
-          </v-card>
-        </v-col>
-
-        <v-col v-for="link in links" :key="link.href" cols="6">
-          <v-card
-            append-icon="mdi-open-in-new"
-            class="py-4"
-            color="surface-variant"
-            :href="link.href"
             :prepend-icon="link.icon"
-            rel="noopener noreferrer"
             rounded="lg"
             :subtitle="link.subtitle"
-            target="_blank"
             :title="link.title"
+            :to="link.route"
             variant="tonal"
           />
         </v-col>
@@ -63,28 +34,22 @@
 <script setup lang="ts">
   const links = [
     {
-      href: 'https://vuetifyjs.com/',
-      icon: 'mdi-text-box-outline',
-      subtitle: 'Learn about all things Vuetify in our documentation.',
-      title: 'Documentation',
+      route: '/Events',
+      icon: 'mdi-calendar',
+      subtitle: 'Upcoming meetings and other events',
+      title: 'Events',
     },
     {
-      href: 'https://vuetifyjs.com/introduction/why-vuetify/#feature-guides',
-      icon: 'mdi-star-circle-outline',
-      subtitle: 'Explore available framework Features.',
-      title: 'Features',
+      route: '/Contact',
+      icon: 'mdi-email',
+      subtitle: 'Contact us or join our mailing list for updates',
+      title: 'Contact',
     },
     {
-      href: 'https://vuetifyjs.com/components/all',
-      icon: 'mdi-widgets-outline',
-      subtitle: 'Discover components in the API Explorer.',
-      title: 'Components',
-    },
-    {
-      href: 'https://discord.vuetifyjs.com',
-      icon: 'mdi-account-group-outline',
-      subtitle: 'Connect with Vuetify developers.',
-      title: 'Community',
+      route: '/Resources',
+      icon: 'mdi-information',
+      subtitle: 'Additional tenant resources and information',
+      title: 'Resources',
     },
   ]
 </script>
